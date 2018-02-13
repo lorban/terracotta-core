6c190c29f4688853d07c1eee3838072479549d4f
Ludovic Orban <lorban@bitronix.be>
2018-02-13 16:36:19 +0100

    change stage queues impl from LinkedBlockingQueue to ArrayBlockingQueue

--- common/src/main/java/com/tc/util/concurrent/QueueFactory.java
+++ common/src/main/java/com/tc/util/concurrent/QueueFactory.java
@@ -18,17 +18,27 @@
  */
 package com.tc.util.concurrent;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import com.tc.async.impl.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class QueueFactory {
 
+  private static final Logger logger = LoggerFactory.getLogger(QueueFactory.class);
+
+  public static final int REASONABLE_MAX_QUEUE_SIZE = 10240;
+
   public <E> BlockingQueue<Event> createInstance(Class<E> type) {
-    return new LinkedBlockingQueue<>();
+    return createInstance(type, REASONABLE_MAX_QUEUE_SIZE + 1);
   }
 
   public <E> BlockingQueue<Event> createInstance(Class<E> type, int capacity) {
-    return new LinkedBlockingQueue<>(capacity);
+    if (capacity > REASONABLE_MAX_QUEUE_SIZE) {
+      logger.info("Queue creation request for {} of max {} entries is not reasonable, clamping at {}", type, capacity, REASONABLE_MAX_QUEUE_SIZE);
+      capacity = REASONABLE_MAX_QUEUE_SIZE;
+    }
+    return new ArrayBlockingQueue<>(capacity);
   }
 }
